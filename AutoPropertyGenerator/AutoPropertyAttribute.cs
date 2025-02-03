4b7a6c5891e02a2cdb3f9c6d35bb88d2bea245b6
ZiedRajhi <50525151+ZiedRajhi@users.noreply.github.com>
2025-02-03 19:16:15 +0100

    new version

---
+++ AutoPropertyGenerator/AutoPropertyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPropertyGenerator
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class AutoPropertyAttribute : Attribute
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# AutoPropertyGenerator
+
+## Description
+Un Source Generator en C# qui ajoute automatiquement `{ get; set; }` aux propriétés des classes marquées avec `[AutoProperty]`.
+
